6cf55f8ccf6b2c5b0f6c90475b02a8b6dd67c8e5
vijayakumar boopathy <learnwithvjk@gmail.com>
2023-10-01 08:54:47 +0530

    SBM-4 feat: added product model

---
+++ product-service/src/main/java/com/learnwithvjk/productservice/model/Product.java
@@ -0,0 +1,24 @@
+package com.learnwithvjk.productservice.model;
+
+import java.math.BigDecimal;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Document
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Product {
+    @Id
+    private String id;
+    private String name;
+    private String description;
+    private BigDecimal price;
+}
